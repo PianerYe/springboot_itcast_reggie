c7fc8ba28a066db78f93b3ba804757f28ff8c599
yepianer <aimeinvshen@126.com>
2023-01-01 22:23:25 +0800

    后台系统登录功能_代码开发 实现登录处理逻辑

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.Employee;
 import com.itheima.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +25,31 @@ public class EmployeeController {
      * 员工登录
      * */
     @PostMapping("/login")
-    public R<Employee> login(HttpServletRequest httpServletRequest, @RequestBody Employee employee){
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+        //1.将页面提交的密码password进行md5加密处理
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
 
-        return null;
+        //2.根据页面提交的用户名username查数据库
+        LambdaQueryWrapper<Employee> queryWarpper = new LambdaQueryWrapper<>();
+        queryWarpper.eq(Employee::getUsername,employee.getUsername());
+        Employee emp = employeeService.getOne(queryWarpper);
+
+        //3.如果没有查询到则返回登录失败结果
+        if (emp == null){
+            return R.error("登录失败");
+        }
+
+        //4.密码比对，如果不一致则返回登陆失败结果
+        if (!emp.getPassword().equals(password)){
+            return R.error("登录失败");
+        }
+        //5.查看员工状态，如果为禁用状态，则返回员工已禁用结果
+        if (emp.getStatus() == 0){
+            return R.error("账号已禁用");
+        }
+        //6.登录成功，将员工ID写入session并返回登录成功结果
+        request.getSession().setAttribute("employee",emp.getId());
+        return R.success(emp);
     }
 }
