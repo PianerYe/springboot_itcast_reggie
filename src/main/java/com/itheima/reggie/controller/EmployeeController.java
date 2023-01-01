3e02694e1b20a8abfc1cd05638ebb78e467e72dc
yepianer <aimeinvshen@126.com>
2023-01-01 22:01:41 +0800

    后台系统登录功能_代码开发 书写登录方法处理逻辑

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -1,10 +1,16 @@
 package com.itheima.reggie.controller;
 
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Employee;
 import com.itheima.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
@@ -12,4 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
+
+    /**
+     * 员工登录
+     * */
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest httpServletRequest, @RequestBody Employee employee){
+
+        return null;
+    }
 }
