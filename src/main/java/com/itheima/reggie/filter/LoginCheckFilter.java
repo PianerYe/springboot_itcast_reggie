1ecdfab57f835a3d1fb8da36bad0d2e1b800d756
yepianer <aimeinvshen@126.com>
2023-01-03 06:52:42 +0800

    完善登录功能 代码开发

--- src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
@@ -1,6 +1,9 @@
 package com.itheima.reggie.filter;
 
+import com.alibaba.fastjson.JSON;
+import com.itheima.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -14,11 +17,53 @@ import java.io.IOException;
 @WebFilter(filterName = "loginCheckFilter",urlPatterns = "/*")
 @Slf4j
 public class LoginCheckFilter implements Filter {
+
+    public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        log.info("拦截到请求：{}",request.getRequestURI());
-        filterChain.doFilter(request,response);
+
+        //1.获取本次请求的URI
+        String requestURI = request.getRequestURI();
+
+        //定义不需要处理的请求路径
+        String[] urls = new String[]{
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "/front/**"
+        };
+
+        //2.判断本次请求是否需要处理
+        boolean check = check(urls, requestURI);
+
+        //3.如果不需要处理，则直接放行
+        if (check){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        //4.判断登录状态，如果已登录，则直接放行
+        if (request.getSession().getAttribute("employee") != null){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        //5.如果未登录则返回登录结果
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+        return;
+    }
+
+    /**
+     * 路径匹配，检查本次请求是否需要放行
+     * */
+    public boolean check(String[] urls,String requestURI){
+        for (String url:urls) {
+            boolean match = PATH_MATCHER.match(url, requestURI);
+            if (match){
+                return true;
+            }
+        }
+        return false;
     }
 }
