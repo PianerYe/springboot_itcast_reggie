c152d0425d5f6bd202f8f76c4f710e5deeed74ea
yepianer <aimeinvshen@126.com>
2023-01-03 23:16:04 +0800

    新增员工 完善全局异常器处理并测试

--- src/main/java/com/itheima/reggie/common/GlobalExceptionHandler.java
+++ src/main/java/com/itheima/reggie/common/GlobalExceptionHandler.java
@@ -24,6 +24,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
     public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
         log.info(ex.getMessage());
-        return R.error("失败了");
+
+        if(ex.getMessage().contains("Duplicate entry")){
+            String[] split = ex.getMessage().split(" ");
+            String msg = split[2] + "已存在";
+            return R.error(msg);
+        }
+
+        return R.error("未知错误");
     }
 }
