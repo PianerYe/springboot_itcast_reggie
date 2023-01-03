21e2331641bcb2353062365acfbdb428d3aff7a1
yepianer <aimeinvshen@126.com>
2023-01-03 13:59:02 +0800

    新增员工 编写全局异常处理器

---
+++ src/main/java/com/itheima/reggie/common/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.itheima.reggie.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ * 全局异常处理
+ * */
+
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+@ResponseBody
+@Slf4j
+public class GlobalExceptionHandler {
+
+    /**
+     * 异常处理方法
+     * */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        log.info(ex.getMessage());
+        return R.error("失败了");
+    }
+}
