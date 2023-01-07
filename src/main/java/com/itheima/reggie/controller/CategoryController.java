53ee4d55ae6abf4b85079c899dbe6388fcf87661
yepianer <aimeinvshen@126.com>
2023-01-07 17:19:50 +0800

    新增分类 需求分析，数据模型，代码开发，功能测试

---
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -0,0 +1,53 @@
+package com.itheima.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+/**
+ * 分类管理
+ * */
+@Slf4j
+@RestController
+@RequestMapping("category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 新增分类
+     * */
+    @PostMapping
+    public R<String> save(@RequestBody Category category){
+        log.info("category: {}",category.toString());
+
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
+
+    /**
+     * 菜品信息分页查询
+     * */
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize){
+        log.info("page = {},pageSize = {}",page,pageSize);
+
+        //构造分页构造器
+        Page pageInfo = new Page(page,pageSize);
+        //构造条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper();
+        //添加过滤条件
+//        queryWrapper.like(StringUtils.isNotEmpty(name),Category::getName,name);//模糊查询，利用like
+        //添加排序条件
+        queryWrapper.orderByDesc(Category::getUpdateTime);
+        //执行查询
+        categoryService.page(pageInfo,queryWrapper);
+        return R.success(pageInfo);
+    }
+}
