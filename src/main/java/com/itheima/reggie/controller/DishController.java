5d100d22e57e668c789a662c9992fdf40467cccf
yepianer <aimeinvshen@126.com>
2023-01-12 01:04:55 +0800

    新增菜品 代码开发，准备工作，梳理交互过程

--- src/main/java/com/itheima/reggie/controller/DishController.java
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -4,12 +4,16 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.entity.DishFlavor;
+import com.itheima.reggie.service.DishFlavorService;
 import com.itheima.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+/**
+ * 菜品管理
+ * */
 @RestController
 @RequestMapping("dish")
 @Slf4j
@@ -17,6 +21,8 @@ public class DishController {
 
     @Autowired
     private DishService dishService;
+    @Autowired
+    private DishFlavorService dishFlavorService;
     /**
      * 分页查询
      * */
@@ -42,7 +48,6 @@ public class DishController {
     @PostMapping
     public R<String> save(@RequestBody Dish dish){
         log.info("dish: {}",dish.toString());
-
         dishService.save(dish);
         return R.success("添加菜品成功");
     }
