aa0b345580024e2adee456c323b0124e5075a417
yepianer <aimeinvshen@126.com>
2023-01-22 21:00:28 +0800

    套餐管理 分页展示成功

---
+++ src/main/java/com/itheima/reggie/controller/SetmealController.java
@@ -0,0 +1,65 @@
+package com.itheima.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.dto.SetmealDto;
+import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.entity.Setmeal;
+import com.itheima.reggie.service.CategoryService;
+import com.itheima.reggie.service.SetmealService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("setmeal")
+@Slf4j
+public class SetmealController {
+    @Autowired
+    private SetmealService setmealService;
+    @Autowired
+    private CategoryService categoryService;
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize,String name){
+        log.info("page = {},pageSize = {},name = {}",page,pageSize,name);
+        //构造分页构造器
+        Page<Setmeal> pageInfo = new Page<>(page,pageSize);
+        Page<SetmealDto> setmealDtoPage = new Page<>(page,pageSize);
+        //构造条件构造器
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        //添加过滤条件
+        queryWrapper.like(StringUtils.isNotEmpty(name),Setmeal::getName,name);
+        //添加排序条件,根据操作时间降序排序
+        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+        //执行查询
+        setmealService.page(pageInfo,queryWrapper);
+        //对象拷贝
+        BeanUtils.copyProperties(pageInfo,setmealDtoPage,"records");
+
+        List<Setmeal> records = pageInfo.getRecords();
+        List<SetmealDto> list = records.stream().map((item) -> {
+            SetmealDto setmealDto = new SetmealDto();
+            BeanUtils.copyProperties(item, setmealDto);
+            Long categoryId = item.getCategoryId();
+            //根据id查询套餐分类对象
+            Category category = categoryService.getById(categoryId);
+            if (category != null) {
+                String categoryName = category.getName();
+                setmealDto.setCategoryName(categoryName);
+            }
+            return setmealDto;
+        }).collect(Collectors.toList());
+
+        setmealDtoPage.setRecords(list);
+
+        return R.success(setmealDtoPage);
+    }
+}
