db0c521b7aacccaecc00346b7ccf4c773e1e49b9
yepianer <aimeinvshen@126.com>
2023-01-29 23:44:19 +0800

    购物车 准备工作

--- src/main/java/com/itheima/reggie/controller/ShoppingCartController.java
+++ src/main/java/com/itheima/reggie/controller/ShoppingCartController.java
@@ -2,10 +2,10 @@ package com.itheima.reggie.controller;
 
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.ShoppingCart;
+import com.itheima.reggie.service.ShoppingCartService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,8 +14,16 @@ import java.util.List;
 @RequestMapping("shoppingcart")
 public class ShoppingCartController {
 
+    @Autowired
+    private ShoppingCartService shoppingCartService;
     @GetMapping("/list")
     public R<List<ShoppingCart>> list(){
         return null;
+    }
+
+    @PostMapping("/add")
+    public R<String> save(@RequestBody ShoppingCart shoppingCart){
+        shoppingCartService.save(shoppingCart);
+        return R.success("菜品添加成功");
     }
 }
