408b2e016a743fb2e5dd28b44a0594ffd5ecd8fd
yepianer <aimeinvshen@126.com>
2023-01-29 10:21:11 +0800

    手机验证码登录 代码开发，登录校验

--- src/main/java/com/itheima/reggie/controller/UserController.java
+++ src/main/java/com/itheima/reggie/controller/UserController.java
@@ -6,12 +6,12 @@ import com.itheima.reggie.entity.User;
 import com.itheima.reggie.service.UserService;
 import com.itheima.reggie.utils.SMSUtils;
 import com.itheima.reggie.utils.ValidateCodeUtils;
-import com.itheima.reggie.utils.sms.Client;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -31,13 +31,6 @@ public class UserController {
         if (user.getPhone() == null){
             return R.error("手机号不能为空");
         }
-        //判断手机号是否在数据库中，不存在则要求客户先注册
-        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(User::getPhone,user.getPhone());
-        int count = userService.count(queryWrapper);
-        if (count == 0){
-            return R.error("手机号不存在，请申请注册");
-        }
 
         //随机生成一个6位数字验证码
         String code = ValidateCodeUtils.generateValidateCode(6).toString();
@@ -46,5 +39,39 @@ public class UserController {
         //将生成的验证码保存到Session中
         session.setAttribute(user.getPhone(),code);
         return R.success("手机验证码发送成功");
+    }
+
+    @PostMapping("/login")
+    public R<User> login(@RequestBody Map map, HttpSession session){
+
+        log.info(map.toString());
+
+        //获取手机号
+        String phone = map.get("phone").toString();
+
+        //获取验证码
+        String code = map.get("code").toString();
+        //从session获取保存的验证码
+        Object codeSession = session.getAttribute(phone);
+
+        if (codeSession != null && codeSession.equals(code)){
+            //进行验证码的比对，说明登录成功
+
+            LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(User::getPhone,phone);
+            User user = userService.getOne(queryWrapper);
+            if (user == null){
+                //判断当前手机号对应的用户是否为新用户，如果是新用户就自动完成注册
+                user = new User();
+                user.setPhone(phone);
+                user.setStatus(1);
+                userService.save(user);
+            }
+
+            return R.success(user);
+
+        }
+
+        return R.error("登录失败");
     }
 }
