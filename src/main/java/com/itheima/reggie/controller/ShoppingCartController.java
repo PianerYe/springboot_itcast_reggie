943f38816c073949eab53d3c12da43443960af09
yepianer <aimeinvshen@126.com>
2023-01-30 10:07:19 +0800

    购物车 添加购物车_代码开发，BUG不能对菜品口味判断，因为前端代码问题

--- src/main/java/com/itheima/reggie/controller/ShoppingCartController.java
+++ src/main/java/com/itheima/reggie/controller/ShoppingCartController.java
@@ -1,5 +1,6 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.itheima.reggie.common.BaseContext;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.ShoppingCart;
@@ -22,10 +23,39 @@ public class ShoppingCartController {
         return null;
     }
 
+    /**
+     * 添加购物车
+     * */
     @PostMapping("/add")
-    public R<String> save(@RequestBody ShoppingCart shoppingCart){
-        shoppingCart.setUserId(BaseContext.getCurrentId());
-        shoppingCartService.save(shoppingCart);
-        return R.success("菜品添加成功");
+    public R<ShoppingCart> save(@RequestBody ShoppingCart shoppingCart){
+        //设置用户ID，指定当前是哪个用户的购物车数据
+        Long currentId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(currentId);
+        //查询当前菜品或者套餐是否在购物车中
+        Long dishId = shoppingCart.getDishId();
+        LambdaQueryWrapper<ShoppingCart> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(ShoppingCart::getUserId,currentId);
+        if (dishId != null){
+            //添加到购物车的是菜品
+            lambdaQueryWrapper.eq(ShoppingCart::getDishId,shoppingCart.getDishId());
+        }else {
+            //添加到购物车的是套餐
+            lambdaQueryWrapper.eq(ShoppingCart::getSetmealId,shoppingCart.getSetmealId());
+        }
+
+        ShoppingCart cartServiceOne = shoppingCartService.getOne(lambdaQueryWrapper);
+
+        if (cartServiceOne != null){
+            //如果已经存在，就在原来数量基础上+1
+            Integer number = cartServiceOne.getNumber();
+            cartServiceOne.setNumber(number + 1);
+            shoppingCartService.updateById(cartServiceOne);
+        }else {
+            //如果不存在，则添加到购物车，数量默认就是1
+            shoppingCart.setNumber(1);
+            shoppingCartService.save(shoppingCart);
+            cartServiceOne = shoppingCart;
+        }
+        return R.success(cartServiceOne);
     }
 }
