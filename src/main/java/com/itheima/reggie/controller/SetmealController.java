cd8991c2cfda7d98469fa587f62550ad5b7bee17
yepianer <aimeinvshen@126.com>
2023-02-23 22:22:45 +0800

    缓存代码测试完成

--- src/main/java/com/itheima/reggie/controller/SetmealController.java
+++ src/main/java/com/itheima/reggie/controller/SetmealController.java
@@ -17,6 +17,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -74,6 +76,7 @@ public class SetmealController {
      * 添加套餐
      * */
     @PostMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> save(@RequestBody SetmealDto setmealDto){
         log.info("setmealDto: {}",setmealDto.toString());
 
@@ -84,6 +87,7 @@ public class SetmealController {
      * 修改套餐
      * */
     @GetMapping("/{id}")
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<SetmealDto> updata(@PathVariable Long id){
         log.info("根据id查询套餐信息...{}",id);
         SetmealDto setmealDto = setmealService.getByIdWithDish(id);
@@ -117,6 +121,7 @@ public class SetmealController {
 //    }
 
     @DeleteMapping()
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> delete(@RequestParam List<Long> ids){
         log.info("ids:{}",ids);
 
@@ -130,6 +135,7 @@ public class SetmealController {
 
     //**修改套餐禁售状态*//*
     @PostMapping("/status/0")
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<List<Setmeal>> updateStatus0(String ids){
         log.info("传递的ids:{}",ids);
         String[] statusIds = ids.split(",");
@@ -143,6 +149,7 @@ public class SetmealController {
 
     //**修改菜品启售状态*//*
     @PostMapping("/status/1")
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<List<Setmeal>> updateStatus1(String ids){
         log.info("传递的ids:{}",ids);
         String[] statusIds = ids.split(",");
@@ -156,6 +163,7 @@ public class SetmealController {
 
 
     @GetMapping("/list")
+    @Cacheable(value = "setmealCache",key = "#setmeal.categoryId+ '_' + #setmeal.status")
     public R<List<Setmeal>> list(Setmeal setmeal){
         log.info("categoryId:{}",setmeal.getCategoryId());
         //条件构造器
